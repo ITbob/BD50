2686dde2df82d7b691df6a0097856b4645d140a8
rjollet- <renaudjollet@gmail.com>
2014-06-15 21:27:33 +0200

    j'ai enlevé les index unique

--- other/Optimisation/400_create_index.sql
+++ other/Optimisation/400_create_index.sql
@@ -2,28 +2,28 @@
 --       INDEX DE LA TABLE AEROPORT
 -- -----------------------------------------------------------------------------
 
-CREATE UNIQUE INDEX AEROPORT_INDEX ON AEROPORT(NOM_AERO);
+CREATE INDEX AEROPORT_INDEX ON AEROPORT(NOM_AERO);
 
 -- -----------------------------------------------------------------------------
 --       INDEX DE LA TABLE PASSAGER
 -- -----------------------------------------------------------------------------
 
-CREATE UNIQUE INDEX PASSAGER_INDEX ON PASSAGER(NOM_PASS,PRENOM_PASS);
+CREATE INDEX PASSAGER_INDEX ON PASSAGER(NOM_PASS,PRENOM_PASS);
 
 -- -----------------------------------------------------------------------------
 --       INDEX DE LA TABLE EMPLOYE
 -- -----------------------------------------------------------------------------
 
-CREATE UNIQUE INDEX EMPLOYE_INDEX ON EMPLOYE(NOM_EMPL,PREN_EMPL);
+CREATE INDEX EMPLOYE_INDEX ON EMPLOYE(NOM_EMPL,PREN_EMPL);
 
 -- -----------------------------------------------------------------------------
 --       INDEX DE LA TABLE CLIENT
 -- -----------------------------------------------------------------------------
 
-CREATE UNIQUE INDEX CLIENT_INDEX ON CLIENT(NOM_CLI,PREN_CLI);
+CREATE INDEX CLIENT_INDEX ON CLIENT(NOM_CLI,PREN_CLI);
 
 -- -----------------------------------------------------------------------------
 --       INDEX DE LA TABLE VOL_REEL
 -- -----------------------------------------------------------------------------
 
-CREATE UNIQUE INDEX VOL_INDEX ON VOL_REEL(JOUR_VOL,MOIS_VOL,ANNE_VOL);
+CREATE INDEX VOL_INDEX ON VOL_REEL(JOUR_VOL,MOIS_VOL,ANNE_VOL);
