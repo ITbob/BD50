b7c37ee7e409829918da25b60dd08428b9bece61
rjollet- <renaudjollet@gmail.com>
2014-06-17 22:13:46 +0200

    Ajout des .cmd

---
+++ other/Structure/create_structure.sql
@@ -0,0 +1,8 @@
+connect G17_FLIGHT/G17_FLIGHT
+set define off 
+Start 290_drop_table_cascade.sql
+Start 291_purge_recyclebin.sql
+Start 200_create_table.sql
+Start 205_create_table_partitionnement.sql
+Start 210_create_pk.sql
+Start 220_create_fk.sql
